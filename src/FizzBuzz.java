40fa999567e90d5255e8c8f3593d7924e21e617d
Rickie Cashwell <rjc6056@gmail.com>
2017-03-11 12:31:38 -0500

    added Main class and removed continue

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,25 +2,20 @@
  * Created by rickiecashwell on 3/9/17.
  */
 public class FizzBuzz {
-    public static void main(String[] args) {
-    int start = 1;
-    int end = 100;
-    doIt(start, end);
 
-    }
     public static void doIt(int start, int end){
         for (int x = start; x <= end; x++){
             if(x % 5 == 0 && x % 3 == 0){
                 System.out.println("FizzBuzz");
-                continue;
+
             }
-            if(x % 3 == 0){
+           else if(x % 3 == 0){
                 System.out.println("Fizz");
-                continue;
+
             }
-            if(x % 5 == 0){
+           else if(x % 5 == 0){
                 System.out.println("Buzz");
-                continue;
+
             }
 
             System.out.println(x);
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by rickiecashwell on 3/11/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        fizzBuzz.doIt(1, 100);
+        
+
+    }
+
+}
